7577e691c67617faaa1ccd7e6bf5025282511ce1
Romanov Artem <artem.romanov@dunice.ru>
2017-08-09 23:04:56 +0300

    fixing check password

--- node-auth/models/user.js
+++ node-auth/models/user.js
@@ -33,8 +33,8 @@ userSchema.virtual('password')
 
 userSchema.methods.checkPassword = function(password) {
   if (!password) { return false; }
-  if (!this.password.passwordHash) { return false; }
-  return crypto.pbkdf2Sync(password, this.salt, 1, 128, 'sha1') === this.passwordHash;
+  if (!this.passwordHash) { return false; }
+  return crypto.pbkdf2Sync(password, this.salt, 1, 128, 'sha1') == this.passwordHash;
 };
 
 userSchema.methods.toSafeObject = function() {
