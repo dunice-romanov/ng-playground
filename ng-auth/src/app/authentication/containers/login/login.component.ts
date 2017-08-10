9f8648c838846dab7f60e1ea85c30ec9265dfc68
Artem Romanov <a.romanov@dunice.net>
2017-08-10 18:09:33 +0300

    added simple register and login components

--- ng-auth/src/app/authentication/containers/login/login.component.ts
+++ ng-auth/src/app/authentication/containers/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+import { UserService } from '../../../core/_services/user.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +12,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  
+  loginError: boolean = false;
+  loading: boolean = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this._createForm();
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) { return; }
+    this.loginError = false;
+    this.loading = true;
+    const { username, password } = this.loginForm.value;
+
+    this.userService.loginUser(username, password)
+      .subscribe(
+        value => {
+          this.router.navigate(['/register']);
+          this.loading = false;
+        },
+        error => {
+          this.loginError = true;
+          this.loading = false;
+        }
+      )
+
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  notEmpty(control) {
+    return control.value.trim().length ? null : {
+      notEmpty: true,
+    };
+  }
+
+  _createForm() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.compose([Validators.required, this.notEmpty])],
+      password: ['', Validators.compose([Validators.required, this.notEmpty])],
+    })
   }
 
 }
--- ng-auth/src/app/authentication/containers/register/register.component.ts
+++ ng-auth/src/app/authentication/containers/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+import { UserService } from '../../../core/_services/user.service';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +12,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  
+  loginError: boolean = false;
+  loading: boolean = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this._createForm();
   }
 
+  onSubmit() {
+    if (this.loginForm.invalid) { return; }
+    this.loginError = false;
+    this.loading = true;
+    const { username, password } = this.loginForm.value;
+
+    this.userService.registerUser(username, password.passwordOriginal)
+      .subscribe(
+        value => {
+          this.router.navigate(['/login']);
+          this.loading = false;
+        },
+        error => {
+          this.loginError = true;
+          this.loading = false;
+        }
+      )
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  get passwordConfirm() {
+    return this.password.get('passwordConfirm');
+  }
+
+  get passwordOriginal() {
+    return this.password.get('passwordOriginal');
+  }
+
+  inputInvalid(input) {
+    return input.invalid  && (input.dirty || input.touched)
+  }
+
+  notEqual(group) {
+    const { passwordConfirm, passwordOriginal } = group.controls;
+    return passwordConfirm.value === passwordOriginal.value ? 
+      null
+      :
+      { notEqual: true };
+  }
+
+  notEmpty(control) {
+    return control.value.trim().length ? null : {
+      notEmpty: true,
+    };
+  }
+
+  _createForm() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.compose([Validators.required, this.notEmpty])],
+      password: this.formBuilder.group({
+        passwordOriginal: ['', Validators.compose([Validators.required, this.notEmpty])],
+        passwordConfirm: ['', Validators.compose([Validators.required, this.notEmpty])],
+      }, {
+        validator: this.notEqual,
+      })
+    })
+  }
 }
