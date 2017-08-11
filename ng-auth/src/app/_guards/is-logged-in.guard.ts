2af46da8986ed5e7caa4726e3a11e68d53d9c978
Artem Romanov <a.romanov@dunice.net>
2017-08-11 10:49:57 +0300

    added login guards

---
+++ ng-auth/src/app/_guards/is-logged-in.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad } from '@angular/router';
+
+import { UserService } from '../core/_services/user.service';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class CanActivateViaAuthGuard implements CanActivate, CanLoad {
+
+  constructor(
+    private userService: UserService,
+    private router: Router,
+  ) {}
+
+  canActivate() {
+    return this.checkLoggedIn();
+  }
+
+  canLoad(): boolean {
+    console.log('this.checkLoggedIn()', this.checkLoggedIn());
+    return this.checkLoggedIn();
+  }
+
+  checkLoggedIn(): boolean {
+    if (this.userService.isLoggedIn()) {
+      return true;
+    };
+    this.router.navigate(['/login']);
+    return false;
+  }
+
+}
---
+++ ng-auth/src/app/_guards/not-logged-in.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad } from '@angular/router';
+
+import { UserService } from '../core/_services/user.service';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class NotAuthGuard implements CanActivate, CanLoad {
+
+  constructor(
+    private userService: UserService,
+    private router: Router,
+  ) {}
+
+  canActivate() {
+    return this.checkLoggedIn();
+  }
+
+  canLoad(): boolean {
+    console.log('this.checkLoggedIn()', this.checkLoggedIn());
+    return this.checkLoggedIn();
+  }
+
+  checkLoggedIn(): boolean {
+    if (this.userService.isLoggedIn()) {
+      this.router.navigate(['/todos']);
+      return false;
+    };
+    return true;
+  }
+
+}
